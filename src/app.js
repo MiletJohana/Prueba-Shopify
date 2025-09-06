0f7c01eb18fa269f6f598ac746c611040c0dffac
MiletJohana <miletjohana95@gmail.com>
2025-09-06 13:46:11 -0500

    feat(header): implementa efecto sticky con scroll

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Gradiweb is online!!!');
+console.log("Gradiweb is online!!!");
+
+// Sticky header scroll effect
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+
+  if (header) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        header.classList.add("header--scrolled");
+      } else {
+        header.classList.remove("header--scrolled");
+      }
+    });
+  }
+});
